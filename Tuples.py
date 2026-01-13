a48da27a5f0c856b5206960934ef14ba525d5f75
cody415 <aparnadesai65@gmail.com>
2026-01-13 06:56:29 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+my_tuple = (10, 20, 30, 40, 50)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:4])
+
+for item in my_tuple:
+    print(item)
+
+print(30 in my_tuple)
+print(100 in my_tuple)
+print(len(my_tuple))
